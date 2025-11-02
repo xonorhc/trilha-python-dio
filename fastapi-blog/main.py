80efbdf6ab280e02422b7019ff93e6cf55e5cb2d
xonorhc <xonorhc@proton.me>
2025-11-02 12:50:42 -0300

    feat: query parameters

--- fastapi-blog/main.py
+++ fastapi-blog/main.py
@@ -4,9 +4,39 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_db = [
+    {
+        "title": f"Criando uma aplicacao com Django",
+        "date": datetime.now(UTC),
+        "published": False,
+    },
+    {
+        "title": f"Criando uma aplicacao com FastAPI",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": f"Criando uma aplicacao com Flask",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": f"Criando uma aplicacao com Startlett",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+]
+
+
+@app.get("/posts")
+def read_posts(published: bool, skip: int = 0, limit: int = 10):
+    return [
+        post for post in fake_db[skip : skip + limit] if post["published"] is published
+    ]
+
 
 @app.get("/posts/{framework}")
-def read_posts(framework: str):
+def read_framework_posts(framework: str):
     return {
         "posts": [
             {
