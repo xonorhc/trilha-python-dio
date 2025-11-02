bd38eaf7e1339a969777c5d4a0230c46b21adcba
xonorhc <xonorhc@proton.me>
2025-11-02 19:11:16 -0300

    feat: body parameter
    Create class with BaseModel and def to post using body parameter.

--- fastapi-blog/main.py
+++ fastapi-blog/main.py
@@ -1,6 +1,7 @@
 from datetime import UTC, datetime
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -28,8 +29,20 @@ fake_db = [
 ]
 
 
+class Post(BaseModel):
+    title: str
+    date: datetime = datetime.now(UTC)
+    published: bool = False
+
+
+@app.post("/post/", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    fake_db.append(post.model_dump())
+    return post
+
+
 @app.get("/posts")
-def read_posts(published: bool, skip: int = 0, limit: int = 10):
+def read_posts(published: bool = True, skip: int = 0, limit: int = 10):
     return [
         post for post in fake_db[skip : skip + limit] if post["published"] is published
     ]
