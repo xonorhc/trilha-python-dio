3af3ae3b58ff66d7be38b5df40e7f5b0d4defa0b
xonorhc <xonorhc@proton.me>
2025-11-02 12:31:31 -0300

    feat: path parameter

--- fastapi-blog/main.py
+++ fastapi-blog/main.py
@@ -1,8 +1,21 @@
+from datetime import UTC, datetime
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
+@app.get("/posts/{framework}")
+def read_posts(framework: str):
+    return {
+        "posts": [
+            {
+                "title": f"Criando uma aplicacao com {framework}",
+                "date": datetime.now(UTC),
+            },
+            {
+                "title": f"Internacionalizando uma app {framework}",
+                "date": datetime.now(UTC),
+            },
+        ]
+    }
