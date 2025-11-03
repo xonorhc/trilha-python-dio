3e953b1645ae9ed472d31eb5b9b9f2e5044c3fd0
xonorhc <xonorhc@proton.me>
2025-11-03 12:01:03 -0300

    feat: create class Address
    Create class Address with relationship to User

--- integration-with-sqlalchemy/main.py
+++ integration-with-sqlalchemy/main.py
@@ -11,3 +11,24 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     fullname = Column(String)
+
+    # relationship
+    address = relationship("Address", back_populates="user", cascade="all")
+
+    def __repr__(self):
+        return f"User({self.id=}: {self.name=}, {self.fullname=})"
+
+
+class Address(Base):
+    __tablename__ = "user_address"
+
+    # atributes
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email_address = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
+
+    # relationship
+    user = relationship("User", back_populates="address")
+
+    def __repr__(self) -> str:
+        return f"Address({self.id=}: {self.email_address=})"
