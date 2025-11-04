a6322e5a24175e65e582a064abe9a2d98ee013bb
xonorhc <xonorhc@proton.me>
2025-11-04 14:42:32 -0300

    fix: add async/await in get

--- fastapi-blog/controllers/post.py
+++ fastapi-blog/controllers/post.py
@@ -1,5 +1,5 @@
 from database import database
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status
 from models.post import posts
 from schemas.post import PostIn
 from views.post import PostOut
@@ -7,17 +7,16 @@ from views.post import PostOut
 router = APIRouter(prefix="/posts")
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
-def create_post(post: PostIn):
-    return post
-
-
 @router.get("/", response_model=list[PostOut])
-def read_posts(
-    response: Response,
+async def read_posts(
     published: bool = True,
     skip: int = 0,
     limit: int = 10,
 ):
     query = posts.select()
-    return database.fetch_all(query)
+    return await database.fetch_all(query)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
+def create_post(post: PostIn):
+    return post
