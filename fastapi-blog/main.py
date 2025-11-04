6ad5f7292908f5e80fcd8bc8a76bb878b246258f
xonorhc <xonorhc@proton.me>
2025-11-04 14:13:12 -0300

    feat: add engine sqlite3
    Use databases package

--- fastapi-blog/main.py
+++ fastapi-blog/main.py
@@ -1,5 +1,24 @@
+from contextlib import asynccontextmanager
+
+import databases
+import sqlalchemy as sa
 from controllers import post
 from fastapi import FastAPI
 
-app = FastAPI()
+DATABASE_URL = "sqlite:///./db.sqlite3"
+
+database = databases.Database(DATABASE_URL)
+metadata = sa.MetaData()
+engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+metadata.create_all(engine)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(post.router)
