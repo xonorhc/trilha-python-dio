f70b9b3bc036989ca35978888306783fa08b1e68
xonorhc <xonorhc@proton.me>
2025-11-04 14:31:22 -0300

    fix: dont create model

--- fastapi-blog/main.py
+++ fastapi-blog/main.py
@@ -10,12 +10,14 @@ DATABASE_URL = "sqlite:///./db.sqlite3"
 database = databases.Database(DATABASE_URL)
 metadata = sa.MetaData()
 engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-metadata.create_all(engine)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    from models.post import posts
+
     await database.connect()
+    metadata.create_all(engine)
     yield
     await database.disconnect()
 
