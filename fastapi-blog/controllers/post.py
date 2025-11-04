5063843d7036e15914d2c773facbcd4bfde4bf6b
xonorhc <xonorhc@proton.me>
2025-11-04 14:30:33 -0300

    fix: update IO from models

--- fastapi-blog/controllers/post.py
+++ fastapi-blog/controllers/post.py
@@ -1,39 +1,13 @@
-from datetime import UTC, datetime
-from typing import Annotated
-
-from fastapi import APIRouter, Cookie, Header, Response, status
+from fastapi import APIRouter, Response, status
 from schemas.post import PostIn
 from views.post import PostOut
 
 router = APIRouter(prefix="/posts")
 
-fake_db = [
-    {
-        "title": f"Criando uma aplicacao com Django",
-        "date": datetime.now(UTC),
-        "published": False,
-    },
-    {
-        "title": f"Criando uma aplicacao com FastAPI",
-        "date": datetime.now(UTC),
-        "published": True,
-    },
-    {
-        "title": f"Criando uma aplicacao com Flask",
-        "date": datetime.now(UTC),
-        "published": True,
-    },
-    {
-        "title": f"Criando uma aplicacao com Startlett",
-        "date": datetime.now(UTC),
-        "published": True,
-    },
-]
-
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
 def create_post(post: PostIn):
-    fake_db.append(post.model_dump())
+    # fake_db.append(post.model_dump())
     return post
 
 
@@ -43,28 +17,5 @@ def read_posts(
     published: bool = True,
     skip: int = 0,
     limit: int = 10,
-    ads_id: Annotated[str | None, Cookie()] = None,
-    user_agent: Annotated[str | None, Header()] = None,
 ):
-    response.set_cookie(key="user", value="email@mail.com")
-    print(f"Cookie: {ads_id}")
-    print(f"User-agent: {user_agent}")
-    return [
-        post for post in fake_db[skip : skip + limit] if post["published"] is published
-    ]
-
-
-@router.get("/{framework}", response_model=PostOut)
-def read_framework_posts(framework: str):
-    return {
-        "posts": [
-            {
-                "title": f"Criando uma aplicacao com {framework}",
-                "date": datetime.now(UTC),
-            },
-            {
-                "title": f"Internacionalizando uma app {framework}",
-                "date": datetime.now(UTC),
-            },
-        ]
-    }
+    return []
