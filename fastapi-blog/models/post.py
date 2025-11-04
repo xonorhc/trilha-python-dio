0984fdf5f1096b8f28b4be3541fca934b1cb2c23
xonorhc <xonorhc@proton.me>
2025-11-04 14:13:43 -0300

    feat: create model

---
+++ fastapi-blog/models/post.py
@@ -0,0 +1,12 @@
+from main import metadata
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, Table
+
+posts = Table(
+    "posts",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("tittle", String(100), nullable=False, unique=True),
+    Column("published_at", DateTime, nullable=True),
+    Column("published", Boolean, default=False),
+    Column("content", String, nullable=False),
+)
